2b33611eb55bb6de8b514cc9b545697a8ea3a27d
Sunil Patel <sunil.patel@digi-corp.com>
2018-02-28 18:47:51 +0530

    Updated version information in css and js file of the components

--- components/button/js/ripple-effect.js
+++ components/button/js/ripple-effect.js
@@ -1,7 +1,7 @@
 
 /**
  * --------------------------------------------------------------------------
- * Propeller v1.2.0 (http://propeller.in): button.js
+ * Propeller v1.3.0 (http://propeller.in): button.js
  * Copyright 2016-2018 Digicorp, Inc.
  * Licensed under MIT (http://propeller.in/LICENSE)
  * -------------------------------------------------------------------------- 
